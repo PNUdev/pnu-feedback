b3da86716adfa9a89789cfb6eb382cb9e76e6df4
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2020-11-28 23:32:08 +0200

    Added navbar placehoder

--- src/main/java/com/pnu/dev/pnufeedback/exception/ExceptionInterceptor.java
+++ src/main/java/com/pnu/dev/pnufeedback/exception/ExceptionInterceptor.java
@@ -1,5 +1,6 @@
 package com.pnu.dev.pnufeedback.exception;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionInterceptor {
 
     @ExceptionHandler(ServiceException.class)
-    public String handleServiceAdminException(ServiceException serviceException) {
+    public String handleServiceAdminException(ServiceException serviceException, Model model) {
+        model.addAttribute("errorMessage", serviceException.getMessage());
         return "error";
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleException() {
+    public String handleException(Model model) {
+        model.addAttribute("errorMessage", "Внутрішня помилка сервера");
         return "error";
     }
 
---
+++ src/main/resources/templates/error.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <#include "include/coreDependencies.ftl" >
+    <title>Moderator panel</title>
+</head>
+<body>
+    <h1>${errorMessage}</h1> <!-- have to be styled someday -->
+</body>
+</html>
