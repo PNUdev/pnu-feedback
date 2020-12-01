db560e624c5b97684c8ab7f3d27681afdbec1f57
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2020-12-01 23:33:45 +0200

    Base markup skeleton for feedback page

--- src/main/resources/templates/submission/feedback-submission.ftl
+++ src/main/resources/templates/submission/feedback-submission.ftl
@@ -2,6 +2,25 @@
 
 <form method="POST">
 
+    <#list scoreQuestions as scoreQuestion>
+        <div>
+            <div>${scoreQuestion.questionNumber} scoreQuestion.content</div>
+            <div>
+                <#list 1..5 as score>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio${score}"
+                               value="${score}">
+                        <label class="form-check-label" for="inlineRadio${score}">${score}</label>
+                    </div>
+                </#list>
+            </div>
+        </div>
+    </#list>
+    Що ви хотіли б запропонувати покращити?
+    <div class="input-group">
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+    </div>
+    <button class="btn btn-primary">Відправити</button>
 </form>
 
 <#include "../include/footer.ftl">
--- src/test/java/com/pnu/dev/pnufeedback/PnuFeedbackApplicationTests.java
+++ src/test/java/com/pnu/dev/pnufeedback/PnuFeedbackApplicationTests.java
@@ -1,13 +1,16 @@
 package com.pnu.dev.pnufeedback;
 
 import com.pnu.dev.pnufeedback.domain.EducationalProgram;
+import com.pnu.dev.pnufeedback.domain.ScoreQuestion;
 import com.pnu.dev.pnufeedback.domain.StakeholderCategory;
 import com.pnu.dev.pnufeedback.repository.EducationalProgramRepository;
+import com.pnu.dev.pnufeedback.repository.ScoreQuestionRepository;
 import com.pnu.dev.pnufeedback.repository.StakeholderCategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -21,10 +24,13 @@ class PnuFeedbackApplicationTests {
     @Autowired
     private StakeholderCategoryRepository stakeholderCategoryRepository;
 
+    @Autowired
+    private ScoreQuestionRepository scoreQuestionRepository;
+
     @Test
     void dbSetup() {
 
-        List<EducationalProgram> educationalPrograms = IntStream.range(0, 5)
+        List<EducationalProgram> educationalPrograms = IntStream.range(1, 5)
                 .mapToObj(idx -> EducationalProgram.builder()
                         .title("Educational program " + idx)
                         .build())
@@ -32,13 +38,26 @@ class PnuFeedbackApplicationTests {
 
         educationalProgramRepository.saveAll(educationalPrograms);
 
-        List<StakeholderCategory> stakeholderCategories = IntStream.range(0, 5)
+        List<StakeholderCategory> stakeholderCategories = IntStream.range(1, 5)
                 .mapToObj(idx -> StakeholderCategory.builder()
                         .title("Stakeholder category " + idx)
                         .build())
                 .collect(Collectors.toList());
 
         stakeholderCategoryRepository.saveAll(stakeholderCategories);
+
+        List<ScoreQuestion> scoreQuestions = IntStream.range(1, 5)
+                .mapToObj(idx -> IntStream.range(1, 5)
+                        .mapToObj(iidx -> ScoreQuestion.builder()
+                                .questionNumber("" + idx + "." + iidx)
+                                .stakeholderCategoryId((long) idx)
+                                .content("Question + " + idx + " + " + iidx)
+                                .build())
+                        .collect(Collectors.toList()))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        scoreQuestionRepository.saveAll(scoreQuestions);
 
     }
 
