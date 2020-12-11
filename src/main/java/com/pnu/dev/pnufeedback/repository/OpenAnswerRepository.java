84de4e40cb5b220affd89c445d712f690151148d
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-12-11 21:57:16 +0200

    OpenAnswerRepository fix

--- src/main/java/com/pnu/dev/pnufeedback/repository/OpenAnswerRepository.java
+++ src/main/java/com/pnu/dev/pnufeedback/repository/OpenAnswerRepository.java
@@ -2,11 +2,11 @@ package com.pnu.dev.pnufeedback.repository;
 
 import com.pnu.dev.pnufeedback.domain.OpenAnswer;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface OpenAnswerRepository extends PagingAndSortingRepository<OpenAnswer, Long> {
+public interface OpenAnswerRepository extends CrudRepository<OpenAnswer, Long> {
 
     List<OpenAnswer> findAllByReviewedFalse(Pageable pageable);
 
