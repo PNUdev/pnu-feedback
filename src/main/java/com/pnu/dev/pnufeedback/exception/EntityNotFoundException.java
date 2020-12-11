cd0014d2a093df1b6ebd2c363298e22e7dbd7774
MarrMika <slobodjanmarija1@gmail.com>
2020-12-11 18:00:12 +0200

    Removed unused class

--- src/main/java/com/pnu/dev/pnufeedback/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pnu.dev.pnufeedback.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
