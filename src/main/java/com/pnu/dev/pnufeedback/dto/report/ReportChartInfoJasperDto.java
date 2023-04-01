a99b93001880c458f55988e800726a0189f4f4c8
GetmaneGit <71373889+getmane@users.noreply.github.com>
2023-04-02 00:53:36 +0300

    Added custom chart customizer, added question texts to jasper report dto (#45)

--- src/main/java/com/pnu/dev/pnufeedback/dto/report/ReportChartInfoJasperDto.java
+++ src/main/java/com/pnu/dev/pnufeedback/dto/report/ReportChartInfoJasperDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,6 +16,8 @@ public class ReportChartInfoJasperDto {
     private String stakeholderCategoryTitle;
 
     private String questionNumber;
+
+    private List<String> questionTexts;
 
     private Double averageScore;
 
---
+++ src/main/java/com/pnu/dev/pnufeedback/util/ChartTooltipCustomizer.java
@@ -0,0 +1,30 @@
+package com.pnu.dev.pnufeedback.util;
+
+import net.sf.jasperreports.engine.JRChart;
+import net.sf.jasperreports.engine.JRChartCustomizer;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+
+import java.awt.Font;
+
+public class ChartTooltipCustomizer implements JRChartCustomizer {
+
+    @Override
+    public void customize(JFreeChart chart, JRChart jasperChart) {
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        BarRenderer barRenderer = (BarRenderer) plot.getRenderer();
+        Font font = new Font("Calibri", Font.BOLD, 12);
+
+        plot.getDomainAxis().setCategoryLabelPositions(
+                CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 2.5));
+
+        barRenderer.setBaseItemLabelFont(font);
+        barRenderer.setBaseItemLabelsVisible(true);
+        plot.getDomainAxis().setMaximumCategoryLabelWidthRatio(1.0f);
+        plot.getDomainAxis().setMaximumCategoryLabelLines(3);
+        plot.getRangeAxis().setUpperMargin(plot.getRangeAxis().getUpperMargin() + 0.1);
+        barRenderer.setItemMargin(0.0f);
+    }
+}
